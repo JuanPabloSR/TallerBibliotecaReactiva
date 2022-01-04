5149e85527264fd72c7bb7d2f81b4d076eeba78f
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-01-03 21:03:18 -0500

    Inicio BibliotecaWebFlux aplicando reactividad

---
+++ src/main/java/co/com/sofka/BibliotecaReactiva/BibliotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.BibliotecaReactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/BibliotecaReactiva/BibliotecaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.BibliotecaReactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
